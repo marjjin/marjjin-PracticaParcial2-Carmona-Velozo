24e8394d9f961a3210b300f8a27842c2b1c2d955
marjjin <carmona.a.martin@gmail.com>
2025-11-10 18:26:08 -0300

    agrega fetch y style a cards

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import "./style.css";
 import { fetchCards, renderCards } from "./Cards/cards.js";
 export const BASE_URL = "https://dragonball-api.com/api";
 
-
 // Obtener y mostrar las cards
 fetchCards()
   .then((data) => {
