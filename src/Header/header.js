234aeaebbe24330543e0552687e15ebcc8b5917d
= <velozoeduin@gmail.com>
2025-11-11 16:02:46 -0300

    Modificaciones

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,13 +1,5 @@
-
 import "./header.css";
 
 export function initHeader() {
   const logo = document.querySelector(".logo-img");
-
-  // Ejemplo: animación o efecto al hacer click en el logo
-  if (logo) {
-    logo.addEventListener("click", () => {
-      alert("¡Bienvenido al universo Dragon Ball!");
-    });
-  }
 }
